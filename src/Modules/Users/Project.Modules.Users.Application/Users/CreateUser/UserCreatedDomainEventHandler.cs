77b7910df919e41884e41ae8eda3389c665161f8
Orangekun <amazingritro66@gmail.com>
2025-08-06 16:24:46 +0300

    refactor: Update UserCreatedDomainEventHandler to include ISender for user retrieval

--- src/Modules/Users/Project.Modules.Users.Application/Users/CreateUser/UserCreatedDomainEventHandler.cs
+++ src/Modules/Users/Project.Modules.Users.Application/Users/CreateUser/UserCreatedDomainEventHandler.cs
@@ -1,16 +1,27 @@
+using System.Data.Common;
+using FluentResults;
+using MediatR;
+using Project.Common.Application.Data;
 using Project.Common.Application.Email;
 using Project.Common.Application.Messaging;
+using Project.Modules.Users.Application.Users.GetUser;
 using Project.Modules.Users.Domain.Users;
 
 namespace Project.Modules.Users.Application.Users.CreateUser;
 
 internal sealed class UserCreatedDomainEventHandler(
-    IEmailService emailService)
+    IEmailService emailService,
+    ISender sender)
     : DomainEventHandler<UserCreatedDomainEvent>
 {
     public override async Task HandleAsync(UserCreatedDomainEvent domainEvent, CancellationToken cancellationToken = default)
     {
-        User user = domainEvent.User;
+        Guid userId = domainEvent.UserId;
+
+        Result<UserResponse> userResult = await sender.Send(new GetUserQuery(userId), cancellationToken);
+
+        UserResponse user = userResult.Value;
+
         string templatePath = Path.Combine(AppContext.BaseDirectory, "Templates", "Emails", "WelcomeUser.cshtml");
 
         await emailService.SendTemplateAsync(
