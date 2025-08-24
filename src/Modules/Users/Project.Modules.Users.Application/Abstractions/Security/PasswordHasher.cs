c0c70448ed0aeecdb0b4da4557f790b36e2a02f4
Youssef Tarek <amazingritro66@gmail.com>
2025-08-24 10:21:09 +0300

    refactor(password-hasher): fix namespace declaration and streamline method definitions

--- src/Modules/Users/Project.Modules.Users.Application/Abstractions/Security/PasswordHasher.cs
+++ src/Modules/Users/Project.Modules.Users.Application/Abstractions/Security/PasswordHasher.cs
@@ -1,15 +1,14 @@
-namespace Project.Modules.Users.Application.Abstractions.Security
-{
-    public class PasswordHasher : IPasswordHasher
-    {
-        public string HashPassword(string password)
-        {
-            return BCrypt.Net.BCrypt.HashPassword(password);
-        }
+namespace Project.Modules.Users.Application.Abstractions.Security;
 
-        public bool VerifyHashedPassword(string hashedPassword, string providedPassword)
-        {
-            return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
-        }
+public class PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyHashedPassword(string hashedPassword, string providedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
     }
 }
