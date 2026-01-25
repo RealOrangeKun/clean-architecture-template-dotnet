b07f158baae5434956a667fff9a680079fe13e50
Youssef Tarek <yousseftarekali04@gmail.com>
2026-01-25 20:11:57 +0200

    feat: add EF Core migration for inbox messages tables

---
+++ src/Modules/Notifications/Project.Modules.Notifications.Infrastructure/Database/Migrations/20260125180810_InboxMessages.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Modules.Notifications.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InboxMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "notifications");
+
+            migrationBuilder.CreateTable(
+                name: "inbox_message_consumers",
+                schema: "notifications",
+                columns: table => new
+                {
+                    inbox_message_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_inbox_message_consumers", x => new { x.inbox_message_id, x.name });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "inbox_messages",
+                schema: "notifications",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "jsonb", maxLength: 2000, nullable: false),
+                    occurred_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    processed_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    error = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_inbox_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_inbox_messages_processed_on_utc_occurred_on_utc",
+                schema: "notifications",
+                table: "inbox_messages",
+                columns: new[] { "processed_on_utc", "occurred_on_utc" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "inbox_message_consumers",
+                schema: "notifications");
+
+            migrationBuilder.DropTable(
+                name: "inbox_messages",
+                schema: "notifications");
+        }
+    }
+}
